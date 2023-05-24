f058cd24c94abad682ff079ea790e925b9433538
Valentyn Naboka <valentin.naboka@gmail.com>
2023-05-24 17:29:43 +0300

    Added dummy test for coverage report

--- data/fetcher_test.go
+++ data/fetcher_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func FetchPagesTest(t *testing.T) {
+func TestFetchPages(t *testing.T) {
 	_, err := FetchPages("", Request{})
 	if err != nil {
 		t.Error("error is not expected")
